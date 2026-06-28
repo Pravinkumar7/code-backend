91d9db817408181f8aaf8d40ef18c2e0000ee797
Pravin Rathod <134314464+Pravinkumar7@users.noreply.github.com>
2026-06-28 12:14:54 +0530

    connect to mongo Atlus

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import dotenv from "dotenv"
+
+import connectDB from "./db/index.js";
+
+dotenv.config()
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+
+const app = express()
+
+
+( async ()=>{
+    try {
+        
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error",()=>{
+        console.log("ERROR: ",error)
+        throw error
+       })
+    
+       app.listen(process.env.PORT, ()=>{
+          console.log(`server is running at http://localhost:${process.env.PORT}`)
+       })
+
+
+    } catch (error) {
+        console.log("ERROR: ",error)
+        throw error 
+        
+    }
+})()
+
+*/
